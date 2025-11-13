b4c63b1de6e0a7392d32815460af34d506cb7673
ZakisCodes <zakinabdul.tech@gmail.com>
2025-11-13 20:22:44 +0530

    Add main.py with lifespan and basic CORS implementation

---
+++ backend/src/main.py
@@ -0,0 +1,47 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .db.base import Base, get_engine    
+from .api.routers import api_router
+
+#lifespan event handler
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #Startup code
+    Base.metadata.create_all(bind= get_engine())
+    yield
+    #Shutdown code
+    get_engine().dispose()
+
+app=FastAPI(
+    title="Attendance Reg",
+    description="Backend service for attendance register full stack web app",
+    lifespan=lifespan,
+)
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/")
+def message():
+    return {"message": "Go to docs endpoint"}
+
+app.include_router(api_router)
+
+
+
+#TODO API versioning
+
